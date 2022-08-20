d5855301e2e7db989c2af442c10cd6f295e8ec81
yangyang <ibelieveing@gmail.com>
2022-08-21 00:05:15 +0800

    update

--- zhangting.py
+++ zhangting.py
@@ -4,7 +4,7 @@ import time
 tool_trade_date_hist_sina_df = ak.tool_trade_date_hist_sina()
 
 trade_date_list = []
-for y in range(2022, 2023):
+for y in range(2020, 2023):
     for index, row in tool_trade_date_hist_sina_df.iterrows():
         trade_date = "{}{:0>2d}{:0>2d}".format(row["trade_date"].year, row["trade_date"].month, row["trade_date"].day)
         #print(type(row["trade_date"].year))
@@ -15,12 +15,11 @@ for y in range(2022, 2023):
     df = pd.DataFrame()
     for day in trade_date_list:
         zhangting_df = ak.stock_zt_pool_em(date=day)
-        time.sleep(1)
         print(day)
-        print(zhangting_df)
-        zhangting_df['date'] = day
         #print(zhangting_df)
-        #for index, row in zhangting_df.iterrows():
-        #    print(row)
+        zhangting_df['date'] = day
+        print(zhangting_df)
+        for index, row in zhangting_df.iterrows():
+            print(row)
         df = pd.concat([df, zhangting_df])
     df.to_csv("zhangting_{}.csv".format(y))
